e52da4dc7445f2d551b5ecda16759bdb779b494d
Matthew Yu <matthewwangyu@gmail.com>
2024-10-08 00:39:24 -0400

    feat: started new project

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+const App = () => {
+  return (
+    <div>
+      <Router>
+        <p>Hello World</p>
+
+        <Routes></Routes>
+      </Router>
+    </div>
+  );
+};
+
+export { App };
